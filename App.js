0cf97937f8df79afadf6c8564b1373ab013d398f
MathieuPlateforme <mathieu.ruiz@laplateforme.io>
2023-01-06 13:44:49 +0100

    API

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import LandingScreen from './src/Screens/ScreenHome';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { API_URL } from './api';
 
 export default function App() {
+
+  const [data, setData] = useState(null);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      axios.get(`${API_URL}/articles?page=1`)
+      .then(res => {
+        setData(res.data);
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    };
+    fetchData();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <LandingScreen />
+      <View>
+      {data ? (
+      data['hydra:member'].map(item => (
+        <Text>{item.title}</Text>
+        ))
+      ) : (
+        <Text>Loading...</Text>
+      )}
+    </View>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
---
+++ api.js
@@ -0,0 +1,1 @@
+export const API_URL = 'http://10.10.18.154:8000/api';
