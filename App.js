cfcce06bddc56ed964ce408eb54ed5a8b49541b8
Reurti13 <82392871+Reurti13@users.noreply.github.com>
2023-01-06 09:39:38 +0100

    add src

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import LandingScreen from './src/Screens/ScreenHome';
+import { StyleSheet } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.txt}>Bienvenue les copain, il est temps de se mettre au boulot !</Text>
+      <LandingScreen />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -16,8 +17,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     backgroundColor: "#0077B6",
     padding: 20
-  },
-  txt: {
-    fontSize: 40
   }
 })
