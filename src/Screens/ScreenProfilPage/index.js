d23b0ba5c73952c597e71f3da2bc19799bb6b0be
Reurti13 <82392871+Reurti13@users.noreply.github.com>
2023-01-09 14:36:22 +0100

    StyleSeet

--- src/Screens/ScreenProfilPage/index.js
+++ src/Screens/ScreenProfilPage/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function ProfilScreen({ navigation }) {
