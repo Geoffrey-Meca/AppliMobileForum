b2360ba2d45b29df10d4e87f319932ba0eab40a2
Calmes <christophe.calmes@laplateforme.io>
2023-01-09 15:30:32 +0100

    footer

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import IndexArticleScreen from './src/Screens/ScreenIndexArticles';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
