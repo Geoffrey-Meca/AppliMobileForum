ca5b0571c77250dea07239f58fb0d5d3bdcaadd9
Calmes <christophe.calmes@laplateforme.io>
2023-01-11 10:32:43 +0100

    Fin inscription

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import jwt_decode from 'jwt-decode';
 import LandingScreen from './src/Screens/ScreenHome';
 import ProfilScreen from './src/Screens/ScreenProfilPage';
 import IndexArticleScreen from './src/Screens/ScreenIndexArticles';
+// Importation de la page d'inscription
+import InscriptionScreen from './src/Screens/ScreenInscription';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -54,6 +56,7 @@ export default function App() {
         <Stack.Screen name="Home" component={LandingScreen} />
         <Stack.Screen name="Articles" component={IndexArticleScreen} />
         <Stack.Screen name="Profil" component={ProfilScreen} />
+        <Stack.Screen name="Inscription" component={InscriptionScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
