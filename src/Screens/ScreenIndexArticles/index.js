cf895006862050ba35a52ab55957b39491b69344
Rany Alo <rbge82@gmail.com>
2023-01-12 21:39:53 +0100

    Add pagination

--- src/Screens/ScreenIndexArticles/index.js
+++ src/Screens/ScreenIndexArticles/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Text, StyleSheet, View} from 'react-native'
+import { Text, StyleSheet, View, Button} from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { getArticles, postComment } from '../../../api';
@@ -9,15 +9,18 @@ import Footer from '../../Composants/Footer';
 export default function IndexArticleScreen({ navigation }) {
 
     const [articles, setArticles] = useState('');
+    const [page, setPage] = useState(1);
+
+    const fetchData = () => {
+        getArticles(page, (res) => {
+            setArticles(res.data);
+        });}
+    const pagesNom = Math.ceil(articles['hydra:totalItems']/5);
+    console.log(page);
 
     useEffect(() => {
-        const fetchData = async () => {
-            getArticles(1, (res) => {
-                setArticles(res.data);
-            });
-        };
         fetchData();
-    }, []);
+    }, [page]);
 
     return (
         <SafeAreaView style={styles.container}>
@@ -29,6 +32,41 @@ export default function IndexArticleScreen({ navigation }) {
             ) : (
                 <Text>Loading...</Text>
             )}
+            </View>
+            <View style={styles.pagination}>
+            <Button
+              onPress={() => {
+                setPage(1);
+                fetchData();
+            }}
+              title="<<"
+              color="#841584"
+            />
+                        <Button
+              onPress={() => {
+                setPage(page-1);
+                fetchData();
+            }}
+              title="<"
+              color="#841584"
+            />
+            <Button
+              onPress={() => {
+                setPage(page+1);
+                fetchData();
+            } }
+              title=">"
+              color="#841584"
+            />
+            <Button
+              onPress={() => {
+                setPage(pagesNom);
+                fetchData();
+            } }
+              title=">>"
+              color="#841584"
+            />
+
             </View>
           <Footer/>
         </SafeAreaView>
@@ -51,5 +89,8 @@ const styles = StyleSheet.create({
         fontSize: 20,
         width: "70%",
         marginTop: 20
+    },
+    pagination: {
+        flexDirection: "row"
     }
 })
