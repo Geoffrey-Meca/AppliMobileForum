c5f099486416900c6d0034df31d90c56c4727306
Reurti13 <82392871+Reurti13@users.noreply.github.com>
2023-01-24 15:20:22 +0100

    coucou

--- src/Screens/ScreenInscription/index.js
+++ src/Screens/ScreenInscription/index.js
@@ -94,15 +94,6 @@ export default function InscriptionScreen({ navigation }) {
                     />
                 </View>
             </ScrollView>
-            <ButtonComponent
-                contButon={styles.contenerCenter}
-                button={styles.butonStyle}
-                txtButton={styles.textButon}
-                text={"Inscription"}
-                onPress={handleSubmit}
-            />
-        </View>
-            </ScrollView >
         </SafeAreaView >
     )
 }
