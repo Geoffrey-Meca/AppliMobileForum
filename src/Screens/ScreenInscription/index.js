6b593c604163da5c422bdcf181c37d8db8f05ab5
MathieuPlateforme <mathieu.ruiz@laplateforme.io>
2023-01-27 15:43:08 +0100

    Pauvre Geoffrey

--- src/Screens/ScreenInscription/index.js
+++ src/Screens/ScreenInscription/index.js
@@ -10,38 +10,25 @@ import { postUser, login } from '../../../api';
 
 export default function InscriptionScreen({ navigation }) {
     const emailRegex = /^\S+@\S+\.\S+$/;
-    const [newEmail, setNewEmail] = useState("");
-    const [newPassword, setNewPassword] = useState("");
-    const [newFirstName, setNewFirstName] = useState("");
-    const [newLastName, setNewLastName] = useState("");
-    const onChangeEmail = (val) => {
-        setNewEmail(val)
-    }
-    const onChangePassword = (val) => {
-        setNewPassword(val)
-    }
-    const onChangeFirstName = (val) => {
-        setNewFirstName(val)
-    }
-    const onChangeLastName = (val) => {
-        setNewLastName(val)
-    }
+    const [user, setUser] = useState('')
+
     const handleSubmit = () => {
-        if (emailRegex.test(newEmail)) {
-            postUser(newEmail, newFirstName, newLastName, newPassword, (res => {
+        if (emailRegex.test(user.email)) {
+            postUser(user.email, user.firstName, user.lastName, user.password, (res => {
                 if (res.status != 201) {
                     Alert.alert(`Impossible de crée l'utilisateur`, `${res.data.violations[0].message}`, [{
                         style: 'cancel'
                     }])
                 }
                 else {
-                    login(newEmail, newPassword, (res => {
+                    login(user.email, user.password, (res => {
                         if (res.status != 200) {
                             Alert.alert(`Connexion automatique échoué, veuillez vous connecter`, `${res.data.message}`, [{
                                 style: 'cancel'
                             }])
                         }
                         else {
+                            setUser('')
                             Alert.alert('Votre inscription a bien été prise en compte', 'Bienvenue sur le Forum !')
                             navigation.navigate('Articles', { refresh: true })
                         }
@@ -61,28 +48,28 @@ export default function InscriptionScreen({ navigation }) {
                 <View style={styles.contenerCenter}>
                     <TextInput
                         style={styles.input}
-                        onChangeText={onChangeEmail}
-                        value={newEmail}
+                        onChangeText={(txt) => setUser({ ...user, email: txt })}
+                        value={user ? user.email : ''}
                         placeholder='Email'
                         keyboardType='email-address'
                     />
                     <TextInput
                         style={styles.input}
-                        onChangeText={onChangePassword}
-                        value={newPassword}
+                        onChangeText={(txt) => setUser({ ...user, password: txt })}
+                        value={user ? user.password : ''}
                         placeholder='Password'
                         secureTextEntry={true}
                     />
                     <TextInput
                         style={styles.input}
-                        onChangeText={onChangeFirstName}
-                        value={newFirstName}
+                        onChangeText={(txt) => setUser({ ...user, firstName: txt })}
+                        value={user ? user.firstName : ''}
                         placeholder='FirstName'
                     />
                     <TextInput
                         style={styles.input}
-                        onChangeText={onChangeLastName}
-                        value={newLastName}
+                        onChangeText={(txt) => setUser({ ...user, lastName: txt })}
+                        value={user ? user.lastName : ''}
                         placeholder='LastName'
                     />
 
